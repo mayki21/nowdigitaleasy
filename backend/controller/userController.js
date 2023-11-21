a2a2e563117e14681988b4e854aebec5755da187
mayki21 <mayank.khobragade9891@gmail.com>
2023-11-21 08:33:07 +0530

    completed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -22,4 +22,106 @@ const userAdded=async(req,res)=>{
 
 }
 
-module.exports={userAdded}
+
+const getDetail=async(req,res)=>{
+    try {
+        const data=await userModel.find()
+        return res.status(200).send({data})
+    } catch (error) {
+        return res.status(400).send(error.message)
+    }
+}
+
+
+// const updateDetail=async(req,res)=>{
+//     try {
+//         const upDate=req.body
+//         const upsucess=await 
+//     } catch (error) {
+        
+//     }
+// }
+
+
+// const getSingleUser = async (req, res) => {
+//     try {
+//       const { userId } = req.params;
+//       const user = await User.findById(userId);
+//       if (!user) {
+//         return res.status(404).json({ message: 'User not found' });
+//       }
+//       res.status(200).json({ user });
+//     } catch (error) {
+//       console.error('Error getting user:', error);
+//       res.status(500).json({ message: 'Error getting user' });
+//     }
+//   };
+
+
+const getSingleUser=async(req,res)=>{
+    try {
+        const { userId }=req.params
+        const data=await userModel.findById(userId)
+        if(!data){
+            return res.status(400).send({message:"User not found"})
+        }
+        res.status(200).json({data})
+
+    } catch (error) {
+        res.status(404).send(error.message)
+        
+    }
+}
+
+// const updateUser=async(req,res)=>{
+//     const {userId}=req.params
+//     const data=req.body
+    
+
+// }
+
+exports.updateUser = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const { name, role, email, phoneNumber } = req.body;
+      const updatedUser = await User.findByIdAndUpdate(userId, { name, role, email, phoneNumber }, { new: true });
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+    } catch (error) {
+      console.error('Error updating user:', error);
+      res.status(500).json({ message: 'Error updating user' });
+    }
+  };
+
+  
+const updateUser=async(req,res)=>{
+    try {
+        const {userId}=req.params
+        const {name,role,email,phonenumber}=req.body
+        const updateuser=await userModel.findByIdAndUpdate(userId,{name,role,email,phonenumber},{new:true})
+        if(!updateuser){
+            return res.status(400).send({message:"User not found"})
+        }
+
+        return res.status(200).send({msg:"updated successfully",user:updateuser})
+    } catch (error) {
+        
+        return res.status(404).send({message:error.message})   
+     }
+}
+  
+
+const deleteUser=async(req,res)=>{
+    try {
+        const {userId}=req.params
+        const deluser=await userModel.findByIdAndDelete(userId)
+        return res.status(200).send({msg:"user delete successfully"})
+    } catch (error) {
+        return res.status(400).send(error.message)
+        
+    }
+}
+
+module.exports={userAdded,getDetail,getSingleUser,updateUser,deleteUser}
--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,9 +1,13 @@
 const express=require("express")
-const { userAdded } = require("../controller/userController")
+const { userAdded, getDetail, getSingleUser, updateUser, deleteUser } = require("../controller/userController")
 const userRoute=express.Router()
 
 
 userRoute.post("/add",userAdded)
+userRoute.get("/get",getDetail)
+userRoute.get("/getsingle/:userId",getSingleUser)
+userRoute.patch("/update/:userId",updateUser)
+userRoute.delete("/del/:userId",deleteUser)
 
 
 module.exports=userRoute
